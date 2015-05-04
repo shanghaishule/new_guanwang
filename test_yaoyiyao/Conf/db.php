7caa959c2715e0739c9aa6d6e37b16f104b4b1fa
ranyigit <ranyi_num@163.com>
2015-05-04 11:30:56 +0800

    合并第三部分

---
+++ test_yaoyiyao/Conf/db.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+	
+'DB_TYPE'=>'mysql',
+	
+'DB_HOST'=>'localhost',
+	
+'DB_PORT'=>'3306',
+	
+'DB_NAME'=>'yaoyiyao',
+	
+'DB_USER'=>'root',
+	
+'DB_PWD'=>'3283d22365',
+	
+'DB_PREFIX'=>'tp_',
+
+);
